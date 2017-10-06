0d64bb2b4ba910ab928a334f54fe25b10e4acb99
Hsi-Chen Lee <hsi.lee@inuvo.com>
2017-10-06 09:15:56 +0100

    added Gruntfile and updated package.json dependencies to latest.  Run npm install

---
+++ Gruntfile.js
@@ -0,0 +1,51 @@
+module.exports = function(grunt) {
+
+    grunt.initConfig({
+        webpack: {
+            options: {
+                progress: true
+            },
+            build: {
+                entry: "./src/main.js",
+                output: {
+                    path: "./dist/",
+                    filename: "vmp.js"
+                }
+            }
+        },
+        mochaTest: {
+            test: {
+                options: {
+                    reporter: 'spec',
+                    quiet: false, // Optionally suppress output to standard out (defaults to false)
+                    clearRequireCache: true // Optionally clear the require cache before running tests (defaults to false)
+                },
+                src: ['test/**/*.js']
+            }
+        },
+        uglify: {
+            bundle: {
+                files: {
+                    './scripts/build/dist/bundle.min.js': ['./scripts/build/dev/bundle.js']
+                }
+            }
+        },
+        watch: {
+            js: {
+                files: ['src/**/*.js'],
+                tasks: ['webpack:build','mochaTest'],
+                options: {
+                    spawn: false
+                }
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-watch'); // file system watcher
+    grunt.loadNpmTasks('grunt-webpack'); // call webpack from here instead of the shell
+    grunt.loadNpmTasks('grunt-mocha-test');
+    grunt.loadNpmTasks('grunt-contrib-uglify'); // utility for minifying js files
+
+    grunt.registerTask('default', ['watch']); // make 'grunt watch' and 'grunt' commands the same
+
+};
